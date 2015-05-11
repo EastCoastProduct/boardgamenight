c0637b00e0a042e0b4742d91b49280d08965cf97
Christopher Swenor <cswenor@Christophers-MacBook-Pro.local>
2015-05-11 18:37:42 -0400

    Added cloudfront flush

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var gulp = require('gulp'),
   s3 = require('gulp-s3'),
   gzip = require('gulp-gzip'),
   filter = require('gulp-filter'),
-  fs = require('fs');
+  fs = require('fs'),
+  var cloudfront = require("gulp-cloudfront");
 
 var options = {
   headers: {'Cache-Control': 'max-age=315360000, no-transform, public'},
@@ -16,5 +17,6 @@ var aws = JSON.parse(fs.readFileSync('./aws.json'));
 gulp.task('publish', function(){
   return gulp.src('./src/**')
     .pipe(gzip())
-    .pipe(s3(aws, options));
+    .pipe(s3(aws, options))
+    .pipe(cloudfront(aws));
 });
